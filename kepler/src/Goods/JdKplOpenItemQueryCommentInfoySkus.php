4e7436de18905fca784db31ef1b587203dcbc8fe
smallsea <seaheaker@163.com>
2018-08-28 12:57:50 +0800

    add goods comment list

---
+++ kepler/src/Goods/JdKplOpenItemQueryCommentInfoySkus.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * 评论信息列表
+ * User: smallsea
+ * Date: 2018/8/28
+ * Time: 12:54
+ */
+
+namespace Jd\Kepler\Goods;
+
+
+use Jd\Kepler\KeplerBase;
+
+class JdKplOpenItemQueryCommentInfoySkus extends KeplerBase
+{
+    /**
+     * 多个sku，逗号分隔
+     * @var string
+     */
+    protected $skus;
+
+    /**
+     * @return string
+     */
+    public function getSkus(): string
+    {
+        return $this->skus;
+    }
+
+    /**
+     * @param string $skus
+     */
+    public function setSkus(string $skus): void
+    {
+        $this->skus = $skus;
+    }
+
+    /**
+     * 接口方法
+     * @return string|array|mixed
+     */
+    public function getApiMethod(): string
+    {
+        return "jd.kpl.open.item.querycommentinfoyskus";
+    }
+}
