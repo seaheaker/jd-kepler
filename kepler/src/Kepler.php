0b4a68866a46bb76974bfc57bdb295653635dd94
smallsea <seaheaker@163.com>
2018-08-28 17:44:22 +0800

    添加商品详情接口;

--- kepler/src/Kepler.php
+++ kepler/src/Kepler.php
@@ -13,6 +13,8 @@ use Exception;
 use Jd\Kepler\Goods\JdKeplerItemQueryCategoriesByFid;
 use Jd\Kepler\Goods\JdKeplerXuanPinSearchSku;
 use Jd\Kepler\Goods\JdKplOpenItemQueryCommentInfoySkus;
+use Jd\Kepler\Goods\JingDongNewWareMobileBigFieldGet;
+use Jd\Kepler\Goods\PublicProductBaseQuery;
 use Jd\Kepler\UnionService\JdKeplerOrderGetList;
 use Jd\Kepler\UnionService\JdKeplerServicePromotionGoodsInfo;
 use Jd\Kepler\UnionService\JdKplOpenBatchConvertCpslink;
@@ -51,7 +53,9 @@ class Kepler
         'jd.kpl.open.unionservice.queryimportorders'    => JdKplOpenUnionServiceQueryImportOrders::class,
         'jd.kpl.open.unionservice.queryCommisions'      => JdKplOpenUnionServiceQueryCommisions::class,
         'jd.kpl.open.cps.convert.keplerurl'             => JdKplOpenCpsConvertKeplerUrl::class,
-        'jd.kpl.open.item.querycommentinfoyskus'        => JdKplOpenItemQueryCommentInfoySkus::class
+        'jd.kpl.open.item.querycommentinfoyskus'        => JdKplOpenItemQueryCommentInfoySkus::class,
+        'public.product.base.query'                     => PublicProductBaseQuery::class,
+        'jingdong.new.ware.mobilebigfield.get'          => JingDongNewWareMobileBigFieldGet::class
     ];
 
     public function __construct(array $config = [])
